04c67fadd96fcad2d3880a76e5cf4b199b163d07
tulazis@yandex.ru <tulazis@yandex.ru>
2022-08-10 12:18:10 +0300

    refactoring: extend source data in array

--- lib.php
+++ lib.php
@@ -4,12 +4,12 @@
  * Эти тестовые данные будем использовать во всех заданиях
  */
 $inputArray = [
-    ['id' => 1, 'date' => "12.01.2020", 'name' => "test1"],
-    ['id' => 2, 'date' => "02.05.2020", 'name' => "test2"],
-    ['id' => 4, 'date' => "08.03.2020", 'name' => "test4"],
-    ['id' => 1, 'date' => "22.01.2020", 'name' => "test1"],
-    ['id' => 2, 'date' => "11.11.2020", 'name' => "test4"],
-    ['id' => 3, 'date' => "06.06.2020", 'name' => "test3"],
+    ['id' => 1, 'date' => "12.01.2020 100", 'name' => "test1", 'sort_order' => 100],
+    ['id' => 2, 'date' => "02.05.2020 1", 'name' => "test2", 'sort_order' => 1],
+    ['id' => 4, 'date' => "08.03.2020 12", 'name' => "test4", 'sort_order' => 12],
+    ['id' => 1, 'date' => "22.01.2020 73", 'name' => "test1", 'sort_order' => 73],
+    ['id' => 2, 'date' => "11.11.2020 91", 'name' => "test4", 'sort_order' => 91],
+    ['id' => 3, 'date' => "06.06.2020 56", 'name' => "test3", 'sort_order' => 56],
 ];
 
 
