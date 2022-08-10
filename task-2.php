b9b1d415feb8540eddefac48020b01c20bcbe789
tulazis@yandex.ru <tulazis@yandex.ru>
2022-08-10 12:23:34 +0300

    add calling function for raw array data

--- task-2.php
+++ task-2.php
@@ -4,16 +4,27 @@ require "lib.php";   // получаем массив с данными
 
 $sortedArray = [];
 
+/**
+ * Соритруем уникализированый массив
+ */
 // фильтруем массив, получаем элементы с уникальными id
 $tempArray = array_unique(array_column($inputArray, 'id' ));
 $uniqueArray = array_intersect_key($inputArray, $tempArray );
 $sortedArray = $uniqueArray;
 
 /**
- * получаем столбец по ключу «sort_order»,
+ * ...получаем столбец по ключу «sort_order»,
  * и сортируем массив по этому столбцу
  */
 $keys = array_column($sortedArray, 'sort_order');
 array_multisort($keys, SORT_ASC, $sortedArray);
 
-consoleLog('Sort multilevel array', $sortedArray);
+consoleLog('Unique data: sort multilevel array', $sortedArray);
+
+/**
+ * Сортируем сырой массив
+ */
+$keys = array_column($inputArray, 'sort_order');
+array_multisort($keys, SORT_ASC, $inputArray);
+
+consoleLog('Raw data: sort multilevel array', $inputArray);
