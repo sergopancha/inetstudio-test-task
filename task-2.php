11e940eb2171d9b605359ea3c8e9fe329d3ccf76
tulazis@yandex.ru <tulazis@yandex.ru>
2022-08-10 12:18:39 +0300

    sort multilevel array

--- task-2.php
+++ task-2.php
@@ -2,3 +2,18 @@
 
 require "lib.php";   // получаем массив с данными
 
+$sortedArray = [];
+
+// фильтруем массив, получаем элементы с уникальными id
+$tempArray = array_unique(array_column($inputArray, 'id' ));
+$uniqueArray = array_intersect_key($inputArray, $tempArray );
+$sortedArray = $uniqueArray;
+
+/**
+ * получаем столбец по ключу «sort_order»,
+ * и сортируем массив по этому столбцу
+ */
+$keys = array_column($sortedArray, 'sort_order');
+array_multisort($keys, SORT_ASC, $sortedArray);
+
+consoleLog('Sort multilevel array', $sortedArray);
